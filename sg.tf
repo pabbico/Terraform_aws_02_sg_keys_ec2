0f96ade49bd7981247815bb3cf6804815a4d9496
Admin <admin@Admins-MacBook-Air-2.local>
2025-07-05 09:14:32 +0530

    Initial commit - Terraform VPC + EC2 setup

---
+++ sg.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "allow_ports" {
+  name        = "allow_ports"
+  description = "Allow ports inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-allow-ports"
+  }
+}
+
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ports" {
+  for_each = toset(var.allowed_ports)
+
+  security_group_id = aws_security_group.allow_ports.id
+  cidr_ipv4         = var.my_ip
+  from_port         = each.value
+  to_port           = each.value
+  ip_protocol       = "tcp"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_ports.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 0
+  to_port           = 65535
+
+}
